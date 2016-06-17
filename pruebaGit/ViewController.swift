6164a093e8e6cefbe2e8a0cb97b4bd78b7782763
Marisela Caldera <mariselacaldera@macbook-pro-de-marisela.local>
2016-06-17 10:51:22 -0600

    Initial Commit

---
+++ pruebaGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaGit
+//
+//  Created by Marisela Caldera on 17/06/16.
+//  Copyright © 2016 Marisela Caldera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
